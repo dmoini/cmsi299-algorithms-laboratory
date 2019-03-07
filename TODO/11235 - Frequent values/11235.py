5979d36387229f2f32a358b0db47746f72fdd34d
Donovan Moini <donovanmoini@gmail.com>
2019-03-07 13:54:30 -0800

    Moved frequent values to TODO

---
+++ TODO/11235 - Frequent values/11235.py
@@ -0,0 +1,15 @@
+import sys
+from collections import Counter
+
+while True:
+    try:
+        data = list(map(int, sys.stdin.readline().split()))
+        if len(data) == 1:
+            break
+    except:
+        break
+    values = list(map(int, sys.stdin.readline().split()))
+    for _ in range(data[1]):
+        l, r = list(map(int, sys.stdin.readline().split()))
+        counter = Counter(values[l - 1:r])
+        print(max(counter.values()))
